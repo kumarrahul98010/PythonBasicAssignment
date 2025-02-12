b23528c650ced3b79ecd90cd8794da21fcacea00
kumarrahul98010 <rahul.kumar@cloudkeeper.com>
2025-02-12 07:16:03 +0530

    question 1

--- q1.py
+++ q1.py
@@ -1,3 +1,12 @@
+#Q1. Write a Python program to perform the following: 
+#Validate a given public IP address to check if it follows the correct format (IPv4).
+#Validate a given email address to check if it’s a valid Gmail address, considering:
+#It should contain "@gmail.com".
+#The username before "@gmail.com" should contain only lowercase letters , numbers and permitted symbols.
+#Provide informative error messages for invalid IP or email.
+
+
+
 import re
 import ipaddress
 
@@ -12,6 +21,7 @@ def check_ip(ip):
 
 def check_email(email):
    rule = r"^[a-z0-9._%+-]+@gmail\.com$"
+   rule= r"^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
    return "sahi hai" if re.match(rule, email) else "galat mail id"
 
 def main():
